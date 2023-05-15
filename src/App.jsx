15e9431db2fdf5d9a457d1d01b8ad93df089e10b
Ivan Varella <ivanvarella@gmail.com>
2023-05-15 11:26:39 -0300

    Fetch API local DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3004/products";
+
 function App() {
+  const [produtcs, setProducts] = useState([]);
+
+  // 1- Resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log("Produts: ", produtcs);
+
   return (
     <div className="App">
       {" "}
       <h1>Lista de produtos</h1>{" "}
+      <ul>
+        {produtcs.map((produtcs) => (
+          <li key={produtcs.id}>
+            {produtcs.name} - R${produtcs.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
